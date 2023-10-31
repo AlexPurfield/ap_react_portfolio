f66adfd6cd60a2d3d78c1e077b9ab948eca98647
Alex Purfield <alexpurfield@gmail.com>
2023-10-31 17:25:17 -0600

    added resume and finished contact form- need to add footer and more styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,61 +1,49 @@
 import { useState } from 'react';
 import '../style/Contact.css';
-
-// Here we import a helper function that will check if the email is valid
-import { checkPassword, validateEmail } from '../utils/helpers';
+import { validateEmail } from '../utils/helpers'; // You only need to import validateEmail
 
 function Form() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
   const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState(''); // Corrected the state variable name
+  const [message, setMessage] = useState(''); // Corrected the state variable name
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
+    const { name, value } = e.target;
 
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'userName') {
-      setUserName(inputValue);
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'name') {
+      setName(value); // Corrected the state variable name
     } else {
-      setPassword(inputValue);
+      setMessage(value); // Corrected the state variable name
     }
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
-    if (!checkPassword(password)) {
-      setErrorMessage(
-        `Choose a more secure password for the account: ${userName}`
-      );
+    if (!validateEmail(email) || !name) {
+      setErrorMessage('Email or name is invalid');
       return;
     }
-    alert(`Hello ${userName}`);
 
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setUserName('');
-    setPassword('');
-    setEmail('');
+    if (!message) { // Corrected the check for an empty message
+      setErrorMessage('Please leave a message before submitting!');
+      return;
+    }
+
+    alert(`Thank you for reaching out, ${name}! I will be in contact with you soon about your message.`);
+
+    setErrorMessage(''); // Clear the error message
+    setName(''); // Clear the name input
+    setMessage(''); // Clear the message input
+    setEmail(''); // Clear the email input
   };
 
   return (
-    <div className="container text-center">
-      <h1 className="display-4 text-pink"> Contact Me! {userName}</h1>
+    <div className="container text-left">
+      <h1 className="display-4 text-pink"> Contact Me! </h1>
       <form className="form" onSubmit={handleFormSubmit}>
         <input
           value={email}
@@ -65,20 +53,21 @@ function Form() {
           placeholder="email"
         />
         <input
-          value={userName}
-          name="userName"
+          value={name}
+          name="name"
           onChange={handleInputChange}
           type="text"
-          placeholder="username"
+          placeholder="name"
+          
         />
         <input
-          value={password}
-          name="password"
+          value={message}
+          name="message"
           onChange={handleInputChange}
-          type="password"
-          placeholder="Password"
+          type="text"
+          placeholder="message"
         />
-        <button type="submit">Submit</button>
+        <button className= "light-grey-button" type="submit">Submit</button>
       </form>
       {errorMessage && (
         <div>
@@ -90,3 +79,4 @@ function Form() {
 }
 
 export default Form;
+
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,19 +2,7 @@ export default function Resume() {
     return (
       <div className="container mt-4 about-container">
       <h1 className="display-4 text-pink">My Resume</h1>
-      <p className="lead">
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-        </p>
+      <a href= "../pdf/Alexandra_Resume_2021.pdf" download>Download Resume</a>
       </div>
     );
   }
