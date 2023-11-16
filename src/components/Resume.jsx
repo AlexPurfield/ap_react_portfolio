633547346c96de16b8937b09ec46a489463eaa43
Alex Purfield <alexpurfield@gmail.com>
2023-11-16 16:04:27 -0700

    updated resume again

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,7 @@ export default function Resume() {
     return (
       <div className="container mt-4 about-container">
       <h1 className="display-4 text-pink">My Resume</h1>
-      <a href= "https://1drv.ms/w/s!At5qwjl_-VL4bDeZS_lSby1DbZs?e=dE8hYK" download>Download Resume</a>
+      <a href= "pdf/Alex_Purfield_Resume2023 (13).pdf" download>Download Resume</a>
       </div>
     );
   }
