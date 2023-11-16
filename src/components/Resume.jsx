fccf15966253f5f44ecca345d1ca8323a48c8263
Alex Purfield <alexpurfield@gmail.com>
2023-11-16 16:39:05 -0700

    added proficiencies

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,6 +3,22 @@ export default function Resume() {
       <div className="container mt-4 about-container">
       <h1 className="display-4 text-pink">My Resume</h1>
       <a href= "/Alex_Purfield_Resume2023(final).pdf" download>Download Resume</a>
+      <h2 className="display-4 text-pink">Proficiencies</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>MySQL</li>
+        <li>Sequelize</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li>jQuery</li>
+        <li>Express.js</li>
+        <li>Bootstrap</li>
+        <li>Handlebars</li>
+        <li>React</li>
+      </ul>
       </div>
     );
   }
