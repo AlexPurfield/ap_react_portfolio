f02cc93395773db4f85b14e6fe64119eeba31dbc
Alex Purfield <alexpurfield@gmail.com>
2023-11-16 16:17:00 -0700

    troubleshoot resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,7 @@ export default function Resume() {
     return (
       <div className="container mt-4 about-container">
       <h1 className="display-4 text-pink">My Resume</h1>
-      <a href= "pdf/Alex_Purfield_Resume2023(final).pdf" download>Download Resume</a>
+      <a href= "/Alex_Purfield_Resume2023(final).pdf" download>Download Resume</a>
       </div>
     );
   }
